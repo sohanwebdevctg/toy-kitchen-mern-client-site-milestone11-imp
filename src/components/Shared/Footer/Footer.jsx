60da5b7b5059af6cd457997239b9fa83ba309739
sohanwebdevctg <amirhossainsohan0@gmail.com>
2023-11-11 17:17:54 +0600

    creating footer

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -3,7 +3,26 @@ import './Footer.css';
 const Footer = () => {
   return (
     <div>
-      <h1>this is footer section</h1>
+      {/* footer section start */}
+      <div>
+        <div className='container mx-auto'>
+          <div className='grid md:grid-cols-4 grid-cols-1 gap-10'>
+            <div>
+              <h1>hello word</h1>
+            </div>
+            <div>
+              <h1>hello word</h1>
+            </div>
+            <div>
+              <h1>hello word</h1>
+            </div>
+            <div>
+              <h1>hello word</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* footer section end */}
     </div>
   );
 };
