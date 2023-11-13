1689cf86e55050a969640821518a78bcd1515e06
sohanwebdevctg <amirhossainsohan0@gmail.com>
2023-11-13 23:04:39 +0600

    adding react-tabs and use shopByCategory and set data

---
+++ src/components/Category/Category.jsx
@@ -0,0 +1,13 @@
+import './Category.css';
+
+const Category = ({category}) => {
+  console.log(category)
+  const {_id, }
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+};
+
+export default Category;
--- src/components/ShopCategory/ShopCategory.jsx
+++ src/components/ShopCategory/ShopCategory.jsx
@@ -1,18 +1,96 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './ShopCategory.css';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import Category from '../Category/Category';
 
 const ShopCategory = () => {
 
+  const [categories, setCategories] = useState([])
 
   useEffect(() => {
     fetch('http://localhost:5000/shopByCategory')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => setCategories(data))
   },[])
 
+
+  // filtering categories data
+  const kitchenSink = categories.filter(data => data.category === "kitchenSink");
+  const kitchenPlaySet = categories.filter(data => data.category === "kitchenPlayset");
+  const cookWare = categories.filter(data => data.category === "cookware");
+  const kitchenAccessories = categories.filter(data => data.category === "kitchenAccessories");
+  const miniKitchen = categories.filter(data => data.category === "miniKitchen");
+
+
   return (
-    <div>
-      <h1>hello</h1>
+    <div className='my-5'>
+      {/* react tabs section start */}
+      <Tabs>
+        {/* header section start */}
+        <TabList>
+          <Tab>Kitchen Sink</Tab>
+          <Tab>Kitchen Play Set</Tab>
+          <Tab>Cook Ware</Tab>
+          <Tab>Kitchen Accessories</Tab>
+          <Tab>Mini Kitchen</Tab>
+        </TabList>
+        {/* header section end */}
+
+        {/* body section start */}
+        <TabPanel>
+          <div className='grid md:grid-cols-2 grid-cols-1 md:mt-3 mt-2'>
+            {
+              kitchenSink.map((category) => <Category
+              key={category._id}
+              category={category}
+              ></Category>)
+            }
+          </div>
+        </TabPanel>
+        <TabPanel>
+        <div className='grid md:grid-cols-2 grid-cols-1 md:mt-3 mt-2'>
+            {
+              kitchenPlaySet.map((category) => <Category
+              key={category._id}
+              category={category}
+              ></Category>)
+            }
+          </div>
+        </TabPanel>
+        <TabPanel>
+        <div className='grid md:grid-cols-2 grid-cols-1 md:mt-3 mt-2'>
+            {
+              cookWare.map((category) => <Category
+              key={category._id}
+              category={category}
+              ></Category>)
+            }
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className='grid md:grid-cols-2 grid-cols-1 md:mt-3 mt-2'>
+            {
+              kitchenAccessories.map((category) => <Category
+              key={category._id}
+              category={category}
+              ></Category>)
+            }
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className='grid md:grid-cols-2 grid-cols-1 md:mt-3 mt-2'>
+            {
+              miniKitchen.map((category) => <Category
+              key={category._id}
+              category={category}
+              ></Category>)
+            }
+          </div>
+        </TabPanel>
+        {/* body section start */}
+      </Tabs>
+      {/* react tabs section end */}
     </div>
   );
 };
